42cc4c34ebc72f7463c61fe6e1a17ee98d9a40c5
Noor Latif <noor@latif.se>
2022-03-18 13:31:08 +0100

    project: re-structure files

--- App.js
+++ App.js
@@ -1,51 +1,7 @@
 import React from 'react';
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  StatusBar,
-  Platform,
-  SafeAreaView,
-  View
-} from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { View } from 'react-native';
+import Welcome from './app/screens/Welcome';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#1F1F24",
-        flex: 1,
-      }}
-    >
-      <View
-          style={{
-            backgroundColor: "yellow",
-            flex: 2,
-          }}
-      />
-      <View
-          style={{
-            backgroundColor: "pink",
-            flex: 1,
-          }}
-      />
-      <View
-          style={{
-            backgroundColor: "purple",
-            flex: 1,
-          }}
-      />
-    </View>
-  );
+  return <Welcome />
 }
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-});
