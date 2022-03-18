376f95e2d804f150a0b6106900c2a3860d7e4732
Noor Latif <noor@latif.se>
2022-03-18 14:01:16 +0100

    welcome: add logo

--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native'
 import React from 'react'
+
+const welcome_bg = require("../assets/background.jpg")
+const logo = require("../assets/logo.png")
 
 export default function Welcome() {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/background.jpg")}
+      source={welcome_bg}
     >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={logo} />
+        <Text>Sell what you're done with. Klart!</Text>
+      </View>
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
       </ImageBackground>
@@ -16,16 +23,26 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
+    alignItems:'center'
+  },
+  logo: {
+    height: 200,
+    width: 200
+  },
+  logoContainer: {    
+    position: "absolute",
+    top: "15%",
+    alignItems:'center'
   },
   loginButton: {
     width: "100%",
     height: 45,
-    backgroundColor: "#fc5c65",
+    backgroundColor: "#fc5c65"
   },
   registerButton: {
     width: "100%",
     height: 45,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: "#4ecdc4"
   }
 });
